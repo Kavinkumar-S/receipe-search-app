c71b0b9ab060247e8bb002b63bd794a8767111af
Kavinkumar-S <kavinselvaraj484@gmail.com>
2022-08-01 22:21:09 +0530

    initial push

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+         <section>
+         <div className="container">
+        fdgjk
+          </div>
+          </section> 
+
+        </>
+    );
+};
+
+export default Home;
